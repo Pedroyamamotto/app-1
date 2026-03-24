d29ea5565106fdc3adbf1772d4a74d08c3348908
Pedro Henrique Vieira Barreto <112330570+Masterbarreto@users.noreply.github.com>
2026-03-24 09:20:55 -0300

    feat: update app version to 3.0.0, add assetBundlePatterns, and include web build script

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+
 import { registerRootComponent } from 'expo';
 import Constants from 'expo-constants';
 import { UserProvider } from './context/UserContext';
 import AppNavigator from './navigation/AppNavigator';
+import { useFonts } from 'expo-font';
+import { Ionicons, MaterialIcons, Feather, FontAwesome } from '@expo/vector-icons';
 
 const isExpoGo =
   Constants.appOwnership === 'expo' ||
@@ -20,7 +23,15 @@ if (!isExpoGo) {
 
 // O componente principal agora simplesmente envolve o AppNavigator com o UserProvider.
 // Toda a lógica de qual tela mostrar está dentro do AppNavigator.
+
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    ...Ionicons.font,
+    ...MaterialIcons.font,
+    ...Feather.font,
+    ...FontAwesome.font,
+  });
+  if (!fontsLoaded) return null;
   return (
     <UserProvider>
       <AppNavigator />
